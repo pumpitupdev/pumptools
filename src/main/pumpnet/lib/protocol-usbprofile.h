7b0306734873380a26594533b306f6a262cfabc0
icex2 <djh.icex2@gmail.com>
2021-01-17 01:03:17 +0100

    pumpnet/lib: Rename protocol -> protocol-usbprofile

--- src/main/pumpnet/lib/protocol-usbprofile.h
+++ src/main/pumpnet/lib/protocol-usbprofile.h
@@ -2,45 +2,31 @@
 
 #include <stdint.h>
 
-#include "pumpnet/lib/pumpnet.h"
-
 #define USBPROFILE_ENDPOINT "/usbprofile/v1"
 
-struct pumpnet_lib_get_save_req {
+struct pumpnet_lib_usbprofile_get_save_req {
     uint64_t trace_id;
     uint64_t machine_id;
     uint64_t player_ref_id;
 } __attribute__((__packed__));
 
-struct pumpnet_lib_get_save_resp {
+struct pumpnet_lib_usbprofile_get_save_resp {
     size_t size;
     uint8_t data[];
 } __attribute__((__packed__));
 
-struct pumpnet_lib_get_rank_req {
+struct pumpnet_lib_usbprofile_get_rank_req {
     uint64_t trace_id;
     uint64_t machine_id;
     uint64_t player_ref_id;
 } __attribute__((__packed__));
 
-struct pumpnet_lib_get_rank_resp {
+struct pumpnet_lib_usbprofile_get_rank_resp {
     size_t size;
     uint8_t data[];
 } __attribute__((__packed__));
 
-struct pumpnet_lib_put_save_req {
-    uint64_t trace_id;
-    uint64_t machine_id;
-    uint64_t player_ref_id;
-    size_t size;
-    uint8_t data[];
-} __attribute__((__packed__));
-
-struct pumpnet_lib_put_save_resp {
-
-} __attribute__((__packed__));
-
-struct pumpnet_lib_put_rank_req {
+struct pumpnet_lib_usbprofile_put_save_req {
     uint64_t trace_id;
     uint64_t machine_id;
     uint64_t player_ref_id;
@@ -48,6 +34,18 @@ struct pumpnet_lib_put_rank_req {
     uint8_t data[];
 } __attribute__((__packed__));
 
-struct pumpnet_lib_put_rank_resp {
+struct pumpnet_lib_usbprofile_put_save_resp {
+
+} __attribute__((__packed__));
+
+struct pumpnet_lib_usbprofile_put_rank_req {
+    uint64_t trace_id;
+    uint64_t machine_id;
+    uint64_t player_ref_id;
+    size_t size;
+    uint8_t data[];
+} __attribute__((__packed__));
+
+struct pumpnet_lib_usbprofile_put_rank_resp {
 
 } __attribute__((__packed__));
